357b6105d47c7994db5c998bb41736a02b050bae
InnovativeSumit <pal2024santosh@gmail.com>
2025-08-20 00:03:22 +0530

    Add files via upload

---
+++ Q3.cpp
@@ -0,0 +1,11 @@
+//Caluculate the ASCII value of a character 
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char a;
+    cout << "Enter a character: ";
+    cin >> a;
+    cout << "The ASCII value of the character is: " << a << " is " << int(a)<< endl;  
+    return 0;
+}
