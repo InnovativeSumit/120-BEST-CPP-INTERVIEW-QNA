6b2b9161f001a09ac26d830528ee6d054f0f7aee
InnovativeSumit <pal2024santosh@gmail.com>
2025-08-21 20:26:37 +0530

    Add files via upload

--- Q4.cpp
+++ Q4.cpp
@@ -1,22 +1,56 @@
-// Check a number is Positive no or Negetive no
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-    int num;
-    cout << "Enter a number: ";
-    cin >> num;
-    if (num==0)
-    {
-        cout << num << "is  neither Positive no nor Negetive no" << endl;
-    }
-    else if ( num > 0)
-    {
-        cout << num << " is a Positive no" << endl;
+
+int OddEven(int n){
+    if(n & 1 == 1){
+        cout<<n<<" is a Odd no ";
     }
     else{
-        cout << num << " is a  Negetive no" << endl;
-    }
+        cout<<n<<" is a Even no ";
 
+    }
+}
+
+
+//when we use void 
+
+// void OddEven(int n){
+//     if(n & 1 == 1){
+//         cout << "This no is a Odd no ";
+//     }
+//     else{
+//         cout << "This no is a even no ";
+//     }
+// }
+// int main(){
+//     cout<<"Enter a number : ";
+//     int n;
+//     cin>>n;
+//     OddEven(n);
+//     // res= oddeven(n);
+//     return 0;
+
+// }
+
+
+
+// int oddeven(int n){
+//     if(n%2==0){
+//         cout<<n<<" is a Odd no ";
+//     }
+//     else{
+//         cout<<n<<" is a Even no ";
+
+//     }
+// }
+
+
+int main(){
+    cout<<"Enter a number : ";
+    int n,res;
+    cin>>n;
+    res=OddEven(n);
+    // res= oddeven(n);
     return 0;
+
 }
