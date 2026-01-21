b7a0077410d5a87466d273c5f95a15cc352530dc
InnovativeSumit <pal2024santosh@gmail.com>
2026-01-21 23:10:04 +0530

    ROOTS OF QUADRATIC EQUATIONS

---
+++ Q41.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    float a, b, c, d, root1, root2;
+
+    cout << "Enter coefficients a, b and c: ";
+    cin >> a >> b >> c;
+
+    d = b*b - 4*a*c;
+
+    if (d > 0) {
+        root1 = (-b + sqrt(d)) / (2*a);
+        root2 = (-b - sqrt(d)) / (2*a);
+        cout << "Roots are real and different." << endl;
+        cout << "Root 1 = " << root1 << endl;
+        cout << "Root 2 = " << root2 << endl;
+    }
+    else if (d == 0) {
+        root1 = -b / (2*a);
+        cout << "Roots are real and equal." << endl;
+        cout << "Root = " << root1 << endl;
+    }
+    else {
+        cout << "Roots are complex and imaginary." << endl;
+    }
+
+    return 0;
+}
